07d127dc39df123ef257632f3ee9f39cb01ed2fa
Owen-Kz <lordkz19@gmail.com>
2023-09-13 22:54:34 +0100

    Update createSpaces.js

--- controllers/createSpaces.js
+++ controllers/createSpaces.js
@@ -3,36 +3,36 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 
-const upload = multer();
-const thumbnailsFolderPath = path.join(__dirname, "../../public/userUpload/spaceCovers");
+// const upload = multer();
+// const thumbnailsFolderPath = path.join(__dirname, "../../public/userUpload/spaceCovers");
 
-fs.access(thumbnailsFolderPath, fs.constants.W_OK, (err) => {
-  if (err) {
-    console.error(`The folder '${thumbnailsFolderPath}' is not writable:`, err);
-  } else {
-    console.log(`The folder '${thumbnailsFolderPath}' is writable`);
-  }
-});
+// fs.access(thumbnailsFolderPath, fs.constants.W_OK, (err) => {
+//   if (err) {
+//     console.error(`The folder '${thumbnailsFolderPath}' is not writable:`, err);
+//   } else {
+//     console.log(`The folder '${thumbnailsFolderPath}' is writable`);
+//   }
+// });
 
-const thumbnailStorage = multer.diskStorage({
-  destination: thumbnailsFolderPath,
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1E9);
-    const fileName = file.originalname.split(".")[0];
-    const fileExtension = path.extname(file.originalname);
-    const thumbnailFile = uniqueSuffix + fileExtension;
-    cb(null, thumbnailFile);
-  },
-});
+// const thumbnailStorage = multer.diskStorage({
+//   destination: thumbnailsFolderPath,
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1E9);
+//     const fileName = file.originalname.split(".")[0];
+//     const fileExtension = path.extname(file.originalname);
+//     const thumbnailFile = uniqueSuffix + fileExtension;
+//     cb(null, thumbnailFile);
+//   },
+// });
 
-const thumbnailUpload = multer({ storage: thumbnailStorage }).single("thumbnail");
+// const thumbnailUpload = multer({ storage: thumbnailStorage }).single("thumbnail");
 
 const createSpaces = async (req, res) =>{
   // Use the thumbnailUpload middleware first
 //   thumbnailUpload(req, res, function (err) {
-    if (err) {
-      return res.status(500).send(err);
-    }
+    // if (err) {
+    //   return res.status(500).send(err);
+    // }
     const {spaceTitle, shortDescription, Buffer} = req.body
 
 
