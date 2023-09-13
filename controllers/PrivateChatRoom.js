8d94ec1c7357aed4b2df9bac87d48b6fff1da15c
Owen-Kz <lordkz19@gmail.com>
2023-09-13 05:20:49 +0100

    Update PrivateChatRoom.js

--- controllers/PrivateChatRoom.js
+++ controllers/PrivateChatRoom.js
@@ -4,123 +4,12 @@ const ChatBufferID = [];
 const recentMessagesArray = [];
 
 console.log("SERVER SIDE CHAT WORKING")
-const startNewChatSession = () => {
-  ChatBufferID.length = 0;
-};
-
-
-
-const checkChatBufferExists = async (senderUsername, recipientUsername, bufferGenerated) => {
-console.log("BUFFER EXISTS")
-  return new Promise((resolve, reject) => {
-    const query = "SELECT COUNT(*) AS chat_exists_count FROM chat_buffer WHERE (user_one = ? AND user_two = ?) OR (user_one = ? AND user_two = ?)";
-    db.query(query, [senderUsername, recipientUsername, recipientUsername, senderUsername], (err, result) => {
-      if (err) return reject(err);
-
-      if (result[0]["chat_exists_count"] > 0) {
-        // Chat buffer exists, so fetch the buffer ID
-        db.query("SELECT buffer_generated FROM chat_buffer WHERE (user_one = ? AND user_two = ?) OR (user_one = ? AND user_two = ?)",
-          [senderUsername, recipientUsername, recipientUsername, senderUsername],
-          (err, bufferResult) => {
-            if (err) return reject(err);
-            
-            // Push the buffer ID into the ChatBufferID array
-            ChatBufferID.push({ id: bufferResult[0]["buffer_generated"] });
-            resolve(true);
-          }
-        );
-      } else {
-console.log("BUFFER CREATED")
-
-        createChatBuffer(senderUsername, recipientUsername, bufferGenerated)
-          .then(() => {
-            ChatBufferID.push({ id: bufferGenerated });
-            resolve(true); // Resolve the promise after creating the chat buffer
-          })
-          .catch((err) => {
-            reject(err); // Reject the promise if there's an error
-          });
-      }
-      
-    });
-  });
-};
-
-const createChatBuffer = async (senderUsername, recipientUsername, bufferGenerated) => {
-  
-  return new Promise((resolve, reject) => {
-    const createChatBufferQuery = "INSERT INTO chat_buffer (user_one, user_two, chat_type, buffer_generated) VALUES (?, ?, ?, ?)";
-    db.query(createChatBufferQuery, [senderUsername, recipientUsername, "private_chat", bufferGenerated], (err, result) => {
-      if (err) return reject(err);
-      resolve(result);
-    });
-  });
-};
-
-const fetchMessageHistory = async (senderUsername, recipientUsername) => {
-console.log("FETCH MESSAGE HISTORY")
-
-  return new Promise((resolve, reject) => {
-    const query = "SELECT * FROM messages WHERE (sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?) ORDER BY timestamp ASC";
-    db.query(query, [senderUsername, recipientUsername, recipientUsername, senderUsername], (err, results) => {
-      if (err) return reject(err);
-      resolve(results);
-    });
-  });
-};
-
-const fetchProfileInfo = async (senderUsername, recipientUsername) => {
-  console.log("FETCH PROFILE INFO")
-  return new Promise((resolve, reject) => {
-    const profiles = {};
-
-    const fetchSenderProfileQuery = "SELECT * FROM user_info WHERE username = ?";
-    db.query(fetchSenderProfileQuery, [senderUsername], (err, senderResult) => {
-      if (err) return reject(err);
-      profiles.senderProfileInfo = senderResult[0];
-
-      const fetchRecipientProfileQuery = "SELECT * FROM user_info WHERE username = ?";
-      db.query(fetchRecipientProfileQuery, [recipientUsername], (err, recipientResult) => {
-        if (err) return reject(err);
-        profiles.recipientProfileInfo = recipientResult[0];
-        resolve(profiles);
-      });
-    });
-  });
-};
-
-const fetchRecentMessages = async (senderUsername, recipientUsername) => {
-  return new Promise((resolve, reject) => {
-    console.log("FETCH RECENT")
-      const query = `
-          SELECT m.*
-          FROM messages m
-          JOIN (
-              SELECT buffer, MAX(timestamp) AS max_timestamp
-              FROM messages
-              WHERE recipient_id = ? OR sender_id = ?
-              GROUP BY buffer
-          ) recent ON m.buffer = recent.buffer AND m.timestamp = recent.max_timestamp
-      `;
-      db.query(query, [senderUsername, senderUsername], async (err, recentMessages) => {
-          if (err) return reject(err);
-
-          const recentMessagesArray = recentMessages.map(message => ({
-              Receiver: message.recipient_id,
-              SentBy: message.sender_id,
-              LastMessage: message.content,
-              TimeStamp: message.timestamp
-          }));
-          resolve(recentMessagesArray);
-      });
-  });
-};
 
 const PrivateChatRoom = async (req, res) => {
-  if (req.user) {
+  // if (req.user) {
     // Call this function when you start a new chat session
     startNewChatSession();
-    const senderUsername = req.user.username;
+    const senderUsername = "req.user.usernam";
     const recipientUsername = req.params["username"];
     const messageHistory = [];
     const bufferGenerated = await bcrypt.hash(senderUsername + recipientUsername, 8);
@@ -173,7 +62,7 @@ const PrivateChatRoom = async (req, res) => {
       console.log("Error:", error);
       res.status(500).send("Chats Could not be loaded");
     }
-  }
+  // }
 };
 
 module.exports = PrivateChatRoom;
