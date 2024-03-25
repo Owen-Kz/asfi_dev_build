afd9295597c93cd34fe4d4fa6a3a68825baa2c98
Benson Michael Owen <lordkz19@gmail.com>
2024-03-25 18:43:39 +0100

    Update sendEmail.js

--- controllers/admin/sendEmail.js
+++ controllers/admin/sendEmail.js
@@ -19,7 +19,7 @@ function SendWelcomeEmail(email,fullname, subject, message){
     sgMail
       .send(msg)
       .then(() => {
-        console.log('Welcome Email sent')
+        console.log('Welcome Email sent for Alpha')
             //  res.status(200).json({ message: 'Reset token sent to your email' });
           // res.render("confirmCode", {message:"Code has been Sent to your email", email:email})
       })
