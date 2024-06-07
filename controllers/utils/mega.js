20bd515f389955e8f5d8ae2e93604350b9b07de8
Benson Michael Owen <lordkz19@gmail.com>
2024-06-07 15:29:28 +0100

    Mega uninstalled

--- controllers/utils/mega.js
+++ controllers/utils/mega.js
@@ -1,11 +1,11 @@
-const Mega = require('mega');
+// const Mega = require('mega');
 
-// Your Mega.nz API credentials
-const apiKey = 'YOUR_API_KEY';
-const email = 'bensonmichael';
-const password = '7xB:9TCk5E_xFsN';
+// // Your Mega.nz API credentials
+// const apiKey = 'YOUR_API_KEY';
+// const email = 'bensonmichael';
+// const password = '7xB:9TCk5E_xFsN';
 
-// Initialize the Mega SDK
-const mega = Mega({ email, password });
+// // Initialize the Mega SDK
+// const mega = Mega({ email, password });
 
-module.exports = mega;  
+// module.exports = mega;  
