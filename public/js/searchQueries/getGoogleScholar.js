459fdfffe42612086512a0d58a5916ca3477c0fb
Michael Fúnṣó <michaelaolufunsho@gmail.com>
2025-02-01 23:35:26 +0100

    Added Google Scholar

---
+++ public/js/searchQueries/getGoogleScholar.js
@@ -0,0 +1,26 @@
+
+const googleScholar_container = document.getElementById("googleScholar_container")
+
+const name = document.getElementById("name").value;
+fetch(`/findGoogleScholar?name=${encodeURIComponent(name)}`, ()=>{
+    method:"GET"
+}).then(res => res.json())
+.then(data =>{
+    console.log(data)
+   })
+
+
+
+    //     if (data.error) {
+    //       profileDiv.innerHTML = `<p style="color: red;">Error: ${data.error}</p>`;
+    //     } else {
+    //       profileDiv.innerHTML = `
+    //         <h2>${data.name}</h2>
+    //         <p><strong>Publications:</strong> ${data.publications}</p>
+    //         <p><strong>Citations:</strong> ${data.citations}</p>
+    //       `;
+    //     }
+    //   } catch (error) {
+    //     console.error("Error fetching data:", error);
+    //     profileDiv.innerHTML = `<p style="color: red;">An error occurred. Please try again.</p>`;
+    //   }
