1066bf956d68044adae1f4d85d609625640f1233
Benson Michael Owen <lordkz19@gmail.com>
2025-04-17 14:05:19 +0100

    content for annoucnement

--- controllers/previewAnnouncement.js
+++ controllers/previewAnnouncement.js
@@ -2,7 +2,7 @@ const db = require("../routes/db.config")
 
 const previewAnnouncement = async (req, res) =>{
     try{
-        let  title = "" 
+      
         let timestamp  = "" 
         let content = "[]"
         if(req.user){
@@ -14,9 +14,10 @@ const previewAnnouncement = async (req, res) =>{
                         return res.json({error:"No data found"})
                     }
                     if(data[0]){
-                       title = data[0].title 
+                    //    title = data[0].title 
                        timestamp  = data[0].timestamp 
                         content = data[0].data
+                        console.log(content)
                     return  res.render("previewAnnouncement", {title, timestamp, content:content,success:"data", status:"loggedIn", UserName:req.user.username, accountType:req.user.acct_type, FirstName:req.user.first_name, LastName: req.user.last_name, ProfileImage: req.user.profile_picture, Email:req.user.email, UserFirstname:req.user.first_name, UserLastName:req.user.last_name, Course:"Course", CourseYear:"CourseYer", username:req.user.username, Username:req.user.username, UserName:req.user.username})
                         
                     }else{
