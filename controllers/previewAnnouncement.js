7c060b5a4cd2457569666bab01643c68a02009b3
Benson Michael Owen <lordkz19@gmail.com>
2025-04-17 14:10:35 +0100

    Content added

--- controllers/previewAnnouncement.js
+++ controllers/previewAnnouncement.js
@@ -18,7 +18,6 @@ const previewAnnouncement = async (req, res) =>{
                        title = data[0].title 
                        timestamp  = data[0].timestamp 
                         content = data[0].data
-                        console.log(content)
                     return  res.render("previewAnnouncement", {title, timestamp, content:content,success:"data", status:"loggedIn", UserName:req.user.username, accountType:req.user.acct_type, FirstName:req.user.first_name, LastName: req.user.last_name, ProfileImage: req.user.profile_picture, Email:req.user.email, UserFirstname:req.user.first_name, UserLastName:req.user.last_name, Course:"Course", CourseYear:"CourseYer", username:req.user.username, Username:req.user.username, UserName:req.user.username})
                         
                     }else{
