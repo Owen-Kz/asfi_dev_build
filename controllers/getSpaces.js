8687c22fe0261b4243982a99cd395e02864c32db
Benson Michael Owen <lordkz19@gmail.com>
2025-04-27 01:23:21 +0100

    Space arranged in order

--- controllers/getSpaces.js
+++ controllers/getSpaces.js
@@ -1,16 +1,32 @@
 const db = require("../routes/db.config");
 
-const getSpaces = async (req,res) =>{
-   
-   if(req.user){ 
-          const username = req.user.username
-      db.query("SELECT * FROM spaces WHERE 1 AND isFromPoster != ?",["true"], async(err, spacesData)=>{
-        if(err) throw err
-        if(spacesData[0]){
-        res.json({message:"Spaces Found", spacesArray: spacesData})  
-        }
-      })
-    }
-}
+const getSpaces = (req, res) => {
+  try {
+    if (req.user) {
+      const username = req.user.username;
+      db.query(
+        "SELECT * FROM spaces WHERE isFromPoster != ? ORDER BY space_focus ASC",
+        ["true"],
+        (err, spacesData) => {
+          if (err) {
+            console.error(err);
+            return res.json({ error: "Database Error", spacesArray: [] });
+          }
 
-module.exports = getSpaces
+          if (spacesData.length > 0) {
+            res.json({ message: "Spaces Found", spacesArray: spacesData });
+          } else {
+            res.json({ message: "No Spaces Found", spacesArray: [] });
+          }
+        }
+      );
+    } else {
+      res.json({ error: "User Not Logged In", spacesArray: [] });
+    }
+  } catch (error) {
+    console.error(error);
+    res.json({ error: error.message, spacesArray: [] });
+  }
+};
+
+module.exports = getSpaces;
